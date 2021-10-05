cc7418e3cda7ae454be7b2e0547a6101415fbc73
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-05 15:54:02 +0800

    :construction: Add early search feature

---
+++ src/routes/search.ts
@@ -0,0 +1,31 @@
+import { app } from '..';
+import Logger from '../logger';
+import ResponseData from '../objects/response-data';
+import Product from '../database/models/Product';
+
+const logger = Logger('Search');
+
+app.get('/search', async (req, res) => {
+  if (!req.isAuthenticated) {
+    res.status(401).json(new ResponseData(true, 'You are not logged in!'));
+    return;
+  }
+
+  if (!req.body.searchTerm) {
+    res.status(400).json(new ResponseData(true, 'Search term is missing!'));
+    return;
+  }
+
+  const products = await Product.query()
+    .withSchema(req.tokenData!.region.replace(/ /g, '_').toUpperCase())
+    .whereRaw("description like '%??%'", [req.body.searchTerm])
+    .orderBy('updatedAt')
+    .select(['name', 'price', 'description', 'id'])
+    .page(req.body.pageNumber ?? 0, 10);
+
+  logger.log(
+    `Returning ${products.results.length} products for user ${req.tokenData?.email} @ ${req.tokenData?.region} (out of a total of ${products.total})`
+  );
+
+  res.json(products);
+});
