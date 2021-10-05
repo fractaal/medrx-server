4e20e8353ba0197d94ec5c194cf9fa1e96f0dd07
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-05 18:39:09 +0800

    :bug: Fix require loop bug by adding modelPaths

--- src/database/models/Base.ts
+++ src/database/models/Base.ts
@@ -1,11 +1,16 @@
 import { Model } from 'objection';
 import { knex } from '..';
+import path from 'path';
 
 Model.knex(knex);
 
 export class Base extends Model {
   updatedAt!: string;
   createdAt!: string;
+
+  static get modelPaths() {
+    return [__dirname];
+  }
 
   $beforeUpdate() {
     this.updatedAt = new Date().toISOString();
