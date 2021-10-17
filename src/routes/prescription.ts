8dc5666b57b3a8812a619c247803d60766b9ff36
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-17 20:58:41 +0800

    :construction: Create prescriptions route

---
+++ src/routes/prescription.ts
@@ -0,0 +1,8 @@
+import { app } from '..';
+import Logger from '../logger';
+import ResponseData from '../objects/response-data';
+import Product from '../database/models/Product';
+
+const logger = Logger('Prescription');
+
+// TODO: Actually do this
