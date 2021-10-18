71c6fe95ef54377f19510100fec44ca868f5610d
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-18 23:12:11 +0800

    :card_file_box: Add Prescription model

---
+++ src/database/models/Prescription.ts
@@ -0,0 +1,38 @@
+import { Model } from 'objection';
+import { Base } from './Base';
+
+export default class Prescription extends Base {
+  id!: string;
+  static get tableName() {
+    return 'prescriptions';
+  }
+
+  static get idColumn() {
+    return 'id';
+  }
+
+  static get relationMappings() {
+    return {
+      vendor: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: 'Vendor',
+        join: {
+          from: 'products.vendorId',
+          to: 'vendors.id',
+        },
+      },
+      product: {
+        relation: Model.ManyToManyRelation,
+        modelClass: 'Product',
+        join: {
+          from: 'prescriptions.id',
+          through: {
+            from: 'prescriptionsProducts.prescriptionId',
+            to: 'prescriptionsProducts.productId',
+          },
+          to: 'products.id',
+        },
+      },
+    };
+  }
+}
