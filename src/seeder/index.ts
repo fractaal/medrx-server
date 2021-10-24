71ef10df7a82e02106344176c3d595cc045f2567
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-24 14:55:50 +0800

    :recycle: Make the seeder more exciting

--- src/seeder/index.ts
+++ src/seeder/index.ts
@@ -2,44 +2,90 @@ import Product from '../database/models/Product';
 import Vendor from '../database/models/Vendor';
 import faker from 'faker';
 import Logger from '../logger';
+import fs from 'fs';
+import path from 'path';
+import knex from 'knex';
 
 const log = Logger('Seeder');
 
+const mercuryDesc = fs.readFileSync(path.resolve(__dirname, 'mercury.txt'), { encoding: 'utf-8' });
+const roseDesc = fs.readFileSync(path.resolve(__dirname, 'rose.txt'), { encoding: 'utf-8' });
+const tgpDesc = fs.readFileSync(path.resolve(__dirname, 'tgp.txt'), { encoding: 'utf-8' });
+const watsonsDesc = fs.readFileSync(path.resolve(__dirname, 'watsons.txt'), { encoding: 'utf-8' });
+
 export default async () => {
-  for (let i = 0; i < 5; i++) {
-    const toInsert = {
-      city: 'Cagayan de Oro City',
-      content: faker.company.catchPhrase(),
-      name: faker.company.companyName(),
-      rating: -1,
-    };
+  const hasAlreadyBeenSeeded =
+    (await knex('metadata').withSchema('REGION_X').where({ key: 'seeded' }).first()).value ?? 0;
 
-    log.log('(VENDOR) Inserting', toInsert);
-
-    await Vendor.query().withSchema('REGION_X').insert(toInsert);
+  if (hasAlreadyBeenSeeded) {
+    log.log('Database has already been seeded! Cancelling...');
+    return;
   }
 
-  const ids = await Vendor.query().withSchema('REGION_X').select('id');
+  await Vendor.query()
+    .withSchema('REGION_X')
+    .insert({ name: 'Mercury Drug Store', content: mercuryDesc, city: 'Cagayan de Oro City' });
+  await Vendor.query()
+    .withSchema('REGION_X')
+    .insert({ name: 'Rose Pharmacy', content: roseDesc, city: 'Cagayan de Oro City' });
+  await Vendor.query()
+    .withSchema('REGION_X')
+    .insert({ name: 'The Generics Pharmacy', content: tgpDesc, city: 'Cagayan de Oro City' });
+  await Vendor.query()
+    .withSchema('REGION_X')
+    .insert({ name: 'Watsons', content: watsonsDesc, city: 'Cagayan de Oro City' });
 
-  console.log(ids);
+  const vendors = await Vendor.query().withSchema('REGION_X').select();
+
+  const photoUrls = [
+    'https://admin.americanaddictioncenters.org/wp-content/uploads/2016/02/Pills-3209654.jpg',
+    'https://assets.unilab.com.ph/uploads/Common/Products/Biogesic/Biogesic-Tablet-Product-Shot-New.jpg',
+    'https://www.rosepharmacy.com/ph1/wp-content/uploads/2016/09/67195.jpg',
+    'https://images.theconversation.com/files/256057/original/file-20190129-108364-17hlc1x.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop',
+  ];
+
+  const names = [
+    'Paracetamol 10mg Variation',
+    'Penicillin 28419824mg Variation',
+    'Airbus A320 CFM Leap Variation',
+    'Alnix',
+    'Biogesic',
+    'Centrum',
+    'Cherifer',
+    'Celine',
+    'Pfizer Vaccine',
+    'Moderna Vaccine',
+    'J&J Vaccine',
+    'Astrazeneca Vaccine',
+    'Sinovac Vaccine',
+    'Free 5G Installations',
+    'Aspirin',
+    'OBAMIUM',
+  ];
 
   for (let i = 0; i < 100; i++) {
-    const idx = Math.round(Math.random() * ids.length - 1);
-    const id = ids[idx].id;
-    log.log(`Foreign key ${id}`);
+    const idx = Math.round(Math.random() * vendors.length - 1);
+    const vendor = vendors[idx];
+    log.log(`Foreign key ${vendor.id}`);
+
+    const productName = names[Math.round(Math.random() * names.length - 1)];
+    const photoUrl = photoUrls[Math.round(Math.random() * photoUrls.length - 1)];
 
     const toInsert = {
-      name: faker.commerce.product(),
-      description: faker.commerce.productDescription(),
+      name: `${vendor.name} - ${productName}`,
+      description: `${productName} is ${faker.commerce.productDescription()}`,
+      photoUrl,
       isPrescriptionRequired: false,
       price: Math.round(Math.random() * 1000 + 50),
-      vendorId: id,
+      vendorId: vendor.id,
     };
 
     log.log('(PRODUCT) Inserting', toInsert);
 
     await Product.query().withSchema('REGION_X').insert(toInsert);
   }
+
+  await knex('metadata').withSchema('REGION_X').where({ key: 'seeded' }).first().update({ value: 1 });
 
   log.success('Seed complete');
 };
