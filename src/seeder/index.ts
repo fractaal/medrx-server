3e38722c6084ab9f0ef07a6bb6d674d480070c9c
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-24 23:37:20 +0800

    :sparkles: Add some more images to seeder

--- src/seeder/index.ts
+++ src/seeder/index.ts
@@ -42,41 +42,49 @@ export default async () => {
     'https://assets.unilab.com.ph/uploads/Common/Products/Biogesic/Biogesic-Tablet-Product-Shot-New.jpg',
     'https://www.rosepharmacy.com/ph1/wp-content/uploads/2016/09/67195.jpg',
     'https://images.theconversation.com/files/256057/original/file-20190129-108364-17hlc1x.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWnZD5Zxu4B5L9npIBbT0nrDJB0wcyP4-JqOh-vahp90LdMHnrvwjaCeqeaDelB2-tqrs&usqp=CAU',
+    'https://www.history.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTU3ODc3NjU2MjA5NjYzMzA1/fast-facts-about-the-war-on-drugs.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxIHwxBr5aNCt4y68LmWHog-0sim3YfOVxir4nFutLe36us8WEsjsEala4yOw3UtvnVs0&usqp=CAU',
+    'https://images.news18.com/ibnlive/uploads/2020/09/1599377768_weed.jpeg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnLU7AZjk4QpX2flxtSsL_8hlS1IbRT8BPXA&usqp=CAU',
+    'https://rehabs.com/static/explore/meth-before-and-after-drugs/img/block1_img.png',
+    'https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F22c44eda-ad0f-11e6-a67d-ec9aa6156041.jpg?crop=1500%2C1000%2C0%2C0',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLZpbxPRJNcaePkzq8Oj9eWK5BVlPKbAqvhg&usqp=CAU',
   ];
 
-  const names = [
-    'Paracetamol 10mg Variation',
-    'Penicillin 28419824mg Variation',
-    'Airbus A320 CFM Leap Variation',
-    'Alnix',
-    'Biogesic',
-    'Centrum',
-    'Cherifer',
-    'Celine',
-    'Pfizer Vaccine',
-    'Moderna Vaccine',
-    'J&J Vaccine',
-    'Astrazeneca Vaccine',
-    'Sinovac Vaccine',
-    'Free 5G Installations',
-    'Aspirin',
-    'OBAMIUM',
-  ];
+  // const names = [
+  //   'Paracetamol 10mg Variation',
+  //   'Penicillin 28419824mg Variation',
+  //   'Airbus A320 CFM Leap Variation',
+  //   'Alnix',
+  //   'Biogesic',
+  //   'Centrum',
+  //   'Cherifer',
+  //   'Celine',
+  //   'Pfizer Vaccine',
+  //   'Moderna Vaccine',
+  //   'J&J Vaccine',
+  //   'Astrazeneca Vaccine',
+  //   'Sinovac Vaccine',
+  //   'Free 5G Installations',
+  //   'Aspirin',
+  //   'OBAMIUM',
+  // ];
 
   for (let i = 0; i < 100; i++) {
     const idx = Math.floor(Math.random() * (vendors.length - 1));
     const vendor = vendors[idx];
     log.log(`Foreign key ${vendor.id}`);
 
-    const productName = names[Math.floor(Math.random() * (names.length - 1))];
+    const productName = faker.commerce.productAdjective() + ' ' + faker.commerce.productName();
     const photoUrl = photoUrls[Math.floor(Math.random() * (photoUrls.length - 1))];
 
     const toInsert = {
-      name: `${vendor.name} - ${productName}`,
+      name: productName,
       description: `${productName} is ${faker.commerce.productDescription()}`,
       photoUrl,
       isPrescriptionRequired: false,
-      price: Math.round(Math.random() * 1000 + 50),
+      price: Math.floor(Math.random() * 1000 + 50),
       vendorId: vendor.id,
     };
 
