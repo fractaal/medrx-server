f068d639200f89f97db99cbb5f8af33bef62343f
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-26 04:54:03 +0800

    :recycle: Refactor prescription controller

--- src/routes/prescription.ts
+++ src/routes/prescription.ts
@@ -2,22 +2,36 @@ import { app } from '..';
 import Logger from '../logger';
 import ResponseData from '../objects/response-data';
 import Prescription from '../database/models/Prescription';
+import { Response, Request } from 'express';
 
 const logger = Logger('Prescription');
 
 // TODO: Actually do this
-app.get('/prescription', async (req, res) => {
+
+app.get('/prescription/*', async (req, res, next) => {
   if (!req.isAuthenticated) {
     res.status(401).json(new ResponseData(true, 'You are not logged in!'));
     return;
+  } else {
+    next();
+  }
+});
+
+const controller = async (req: Request, res: Response, latest = false) => {
+  const prescriptionPromise = Prescription.query().withSchema(req.tokenData!.region.replace(/ /g, '_').toUpperCase());
+  // .orderBy('dateCreated')
+  // .first();
+
+  if (latest) {
+    prescriptionPromise.orderBy('dateCreated').first();
+  } else {
+    prescriptionPromise.findById(req.params.id);
   }
 
-  const prescription = await Prescription.query()
-    .withSchema(req.tokenData!.region.replace(/ /g, '_').toUpperCase())
-    .findById(req.tokenData!.uid);
+  const prescription = (await prescriptionPromise) as unknown as Prescription;
 
   if (!prescription) {
-    res.json(new ResponseData(false, 'No prescription for this user', []));
+    res.json(new ResponseData(false, 'No prescription found', []));
     return;
   }
 
@@ -27,4 +41,7 @@ app.get('/prescription', async (req, res) => {
     .select('name', 'vendorId', 'description', 'price', 'photoUrl');
 
   res.json(new ResponseData(false, `Got ${results.length} items`, results));
-});
+};
+
+app.get('/prescription/latest', async (req, res) => controller(req, res, true));
+app.get('/prescription/:id', async (req, res) => controller(req, res));
