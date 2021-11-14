3fe169f37628cf4429e69e41aa3bacd01f19cdaf
Ben Jude <benedict.jude.rocat@gmail.com>
2021-11-14 15:56:53 +0800

    :card_file_box: Add roles property to globals

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,6 +1,6 @@
 namespace Express {
   interface Request {
     isAuthenticated: boolean;
-    tokenData: (import('firebase-admin').auth.DecodedIdToken & { city: string; region: string }) | null;
+    tokenData: (import('firebase-admin').auth.DecodedIdToken & { city: string; region: string; roles: string }) | null;
   }
 }
