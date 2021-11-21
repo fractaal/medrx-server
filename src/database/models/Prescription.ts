65c8d384bc75a1a343b245357c593f945f46d654
Ben Jude <benedict.jude.rocat@gmail.com>
2021-11-22 02:24:24 +0800

    🗃️ Add extra remarks field to model

--- src/database/models/Prescription.ts
+++ src/database/models/Prescription.ts
@@ -5,14 +5,12 @@ import Product from './Product';
 export default class Prescription extends Base {
   id!: string;
   userId!: string;
-
   isValid!: boolean;
   isConfirmed!: boolean;
-
   dateSubmitted!: string;
   dateConfirmedOrCancelled!: string;
-
   products!: any[];
+  extraRemarks!: string;
 
   static get tableName() {
     return 'prescriptions';
