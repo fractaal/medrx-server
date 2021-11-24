24e65ab734f03691edc6d844795e66f5136b8aa0
Ben Jude <benedict.jude.rocat@gmail.com>
2021-11-24 19:05:00 +0800

    ✨ Delivery Request API

---
+++ src/api/delivery-request.ts
@@ -0,0 +1,24 @@
+import { database } from 'firebase-admin';
+import { CartItem } from '../database/models/CartItem';
+import Logger from '../logger';
+
+const logger = Logger('Delivery Request API');
+
+export const createDeliveryRequest = async (userId: string, region: string, city: string, products: CartItem[]) => {
+  logger.log(`Created a new delivery request for ${userId} on ${region}/${city}`);
+  const location = database().ref(`/${region}/${city}/${userId}/deliveryRequests`);
+  location.set({
+    isAccepted: false,
+    products,
+  });
+  return location;
+};
+
+export const getDeliveryRequestDbRef = async (userId: string, region: string, city: string) => {
+  return database().ref(`/${region}/${city}/${userId}/deliveryRequests`);
+};
+
+export const deleteDeliveryRequest = async (userId: string, region: string, city: string) => {
+  const location = await getDeliveryRequestDbRef(userId, region, city);
+  await location.remove();
+};
