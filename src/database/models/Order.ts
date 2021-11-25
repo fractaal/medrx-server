32e9a64600af207a0b759aed47206014b5545661
Ben Jude <benedict.jude.rocat@gmail.com>
2021-11-26 02:10:39 +0800

    🐛 fix erroneous table name

--- src/database/models/Order.ts
+++ src/database/models/Order.ts
@@ -11,7 +11,7 @@ export default class Order extends Base {
   products!: CartItem[];
 
   static get tableName() {
-    return 'prescriptions';
+    return 'orders';
   }
 
   static get idColumn() {
