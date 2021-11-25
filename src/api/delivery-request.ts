32f0a57ab9b60e25e8d01b6d2ed504efcec3ba38
Ben Jude <benedict.jude.rocat@gmail.com>
2021-11-26 02:09:08 +0800

    ✨ claimDeliveryRequest

--- src/api/delivery-request.ts
+++ src/api/delivery-request.ts
@@ -6,7 +6,7 @@ const logger = Logger('Delivery Request API');
 
 export const createDeliveryRequest = async (userId: string, region: string, city: string, products: CartItem[]) => {
   logger.log(`Created a new delivery request for ${userId} on ${region}/${city}`);
-  const location = database().ref(`/${region}/${city}/${userId}/deliveryRequests`);
+  const location = database().ref(`/${region}/${city}/${userId}/deliveries`);
   location.set({
     isAccepted: false,
     products,
@@ -15,7 +15,15 @@ export const createDeliveryRequest = async (userId: string, region: string, city
 };
 
 export const getDeliveryRequestDbRef = async (userId: string, region: string, city: string) => {
-  return database().ref(`/${region}/${city}/${userId}/deliveryRequests`);
+  return database().ref(`/${region}/${city}/${userId}/deliveries`);
+};
+
+export const claimDeliveryRequest = async (claimerId: string, userId: string, region: string, city: string) => {
+  const location = await getDeliveryRequestDbRef(userId, region, city);
+  await location.update({
+    isAccepted: true,
+    claimerId,
+  });
 };
 
 export const deleteDeliveryRequest = async (userId: string, region: string, city: string) => {
