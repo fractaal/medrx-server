0b1b8d27d0fbc24a81d4e55e20ac32ada5decae7
Ben Jude <benedict.jude.rocat@gmail.com>
2021-12-01 17:15:48 +0800

    :sparkles: getDeliveryRequestsByProximity

--- src/api/delivery-request.ts
+++ src/api/delivery-request.ts
@@ -1,12 +1,14 @@
 import { database } from 'firebase-admin';
 import { CartItem } from '../database/models/CartItem';
+import { Delivery } from '../database/models/DeliveryRequest';
 import Logger from '../logger';
+import { getDistance, Location } from '../util/positions';
 
 const logger = Logger('Delivery Request API');
 
 export const createDeliveryRequest = async (userId: string, region: string, city: string, products: CartItem[]) => {
   logger.log(`Created a new delivery request for ${userId} on ${region}/${city}`);
-  const location = database().ref(`/${region}/${city}/${userId}/deliveries`);
+  const location = await getDeliveryRequestDbRef(userId, region, city);
   location.set({
     isAccepted: false,
     products,
@@ -15,7 +17,40 @@ export const createDeliveryRequest = async (userId: string, region: string, city
 };
 
 export const getDeliveryRequestDbRef = async (userId: string, region: string, city: string) => {
-  return database().ref(`/${region}/${city}/${userId}/deliveries`);
+  return database().ref(`/${region}/${city}/${userId}/delivery`);
+};
+
+// TODO: make work
+export const getDeliveryRequestsByProximity = async (
+  origin: Location,
+  maxEntries = 10,
+  maxDistance = 10,
+  region: string,
+  city: string
+) => {
+  const ref = database().ref(`${region}/${city}`);
+  const usersData: Record<string, { delivery: Delivery }> = await (await ref.get()).val();
+
+  for (const [userId, userData] of Object.entries(usersData)) {
+    const { delivery } = userData;
+
+    const distance = getDistance(origin, { lat: delivery.lat, lng: delivery.lng });
+    if (distance > maxDistance) {
+      continue;
+    }
+
+    if (delivery.isAccepted) {
+      continue;
+    }
+
+    return {
+      userId,
+      firstName: delivery.firstName,
+      middleName: delivery.middleName,
+      lastName: delivery.lastName,
+      distance,
+    };
+  }
 };
 
 export const claimDeliveryRequest = async (claimerId: string, userId: string, region: string, city: string) => {
